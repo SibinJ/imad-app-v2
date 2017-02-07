32d507960fae26b0eaec1cdcc18a7ad32a68d449
SibinJ <sibinjacob.7@gmail.com>
2017-02-08 02:22:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,7 @@
-console.log('Loaded!');
+var counter = 0;
+var but = document.getElementById("counter");
+but.onclick = function(){
+    counter = counter + 1;
+    var span = document.getElementById("count");
+    span.innerHTML = counter.toString();
+};
