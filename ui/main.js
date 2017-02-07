68482f8bd9382249c581c01e254dfc4389a15b8a
SibinJ <sibinjacob.7@gmail.com>
2017-02-08 02:25:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 var counter = 0;
-var but = document.getElementById("counter");
+var but = document.getElementById('counter');
 but.onclick = function(){
     counter = counter + 1;
-    var span = document.getElementById("count");
+    var span = document.getElementById('count');
     span.innerHTML = counter.toString();
 };
